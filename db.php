53888d547918831415b5bf52b419a423af3134c8
Mervion6 <mervinsilveriojr@gmail.com>
2026-03-31 12:50:21 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$host = "db";
+$user = "root"; 
+$pass = "root"; 
+$db   = "driptee_db";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
